9b1f561be3722cb5c2b7179e629e5b21b7d49e44
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2022-05-30 21:54:52 -0300

    Exercícios 13.14 e 15 sobre condições e uso de Switch

---
+++ Aula12/exercicio013.js
@@ -0,0 +1,10 @@
+var idade = 25
+console.log(`Você tem ${idade} anos`)
+
+if (idade < 16) {
+    console.log("Não vota")
+} else if (idade < 18 || idade > 65) {
+    console.log("Voto opcional") 
+} else {                        //(idade >= 18) 
+    console.log("Voto obrigatório")
+}
