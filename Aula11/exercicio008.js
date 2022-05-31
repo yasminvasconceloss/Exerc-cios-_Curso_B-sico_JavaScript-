eb824da2c24838f00ca877036dba2e80d82115d5
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2022-05-30 21:52:08 -0300

    Exercícios 8,9,10 e 11 sobre condições do JavaScript

---
+++ Aula11/exercicio008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel} Km/H`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança!')
