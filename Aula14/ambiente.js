f2e8e0eb98b8d42407eb0137c894b924a628d1c7
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2022-06-05 00:32:07 -0300

    Aula 14 (parte 2) - Repetições - uso de For

---
+++ Aula14/ambiente.js
@@ -0,0 +1,16 @@
+/*
+USO DE WHILE 
+var cont = 1
+while (cont <= 8) {
+    console.log('tudo bem')
+    cont++ 
+}
+*/
+
+
+// USO DE FOR 
+// for (inicio; teste; incr)
+
+for (var cont=1; cont<= 8; cont++){
+    console.log(cont)
+}
