bc3c3723be69252515c47d6ec18ffa8e51551a54
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2022-06-04 23:49:13 -0300

    Aula 13 (parte 1) - uso de repetições - While

---
+++ Aula13/ambiente.js
@@ -0,0 +1,35 @@
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
+
+
+/*
+var cont = 1
+while (cont <= 8) {
+    console.log('tudo bem')
+    cont++ 
+}
+*/
+
+/*
+var cont = 1
+while (cont <= 3) {
+    console.log(`Passo ${cont}`)
+    cont++ 
+}
+*/
+
+/*
+var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++ 
+} while (cont <= 8)
+*/
